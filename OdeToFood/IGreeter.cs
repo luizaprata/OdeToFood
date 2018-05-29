a7ebcd96a576fa35829a2820beeded8154ea64df
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2018-05-29 13:53:48 -0300

    first commit - middleware

---
+++ OdeToFood/IGreeter.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetMessageOfTheDay();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private IConfiguration _configuration;
+
+        public Greeter(IConfiguration conf)
+        {
+            _configuration = conf;
+        }
+        public string GetMessageOfTheDay()
+        {
+            return _configuration["Greeting"];
+        }
+    }
+}
