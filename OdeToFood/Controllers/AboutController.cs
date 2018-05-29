3d0cdb18f006fcd83670756ac71dfb90bc5ff9fe
Luiza Prata Soldi Passos <luiza.passos@lambda3.com.br>
2018-05-29 16:35:46 -0300

    controllers

---
+++ OdeToFood/Controllers/AboutController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OdeToFood.Controllers
+{
+    [Route("company/[controller]/[action]")]
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "00 00000-0000";
+        }
+
+        public string Address()
+        {
+            return "BR";
+        }
+    }
+}
--- OdeToFood/Controllers/HomeController.cs
+++ OdeToFood/Controllers/HomeController.cs
@@ -1,19 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using Microsoft.AspNetCore.Mvc;
 
 namespace OdeToFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        // GET: /<controller>/
-        public string Index()
+        protected HomeController()
         {
-            return "hello from home controller";
+        }
+
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return Content("HomeController");
         }
     }
 }
